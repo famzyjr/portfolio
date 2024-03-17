1005760ed622724ff44b1cafc35950857db116ac
famzyjr <famuyiwaahmed132@gmail.com>
2024-03-17 22:18:46 +0100

    fifth commit

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
-import Cert from "../../assets/cert.pdf"
+// import CV from '../../assets/cv.pdf'
+  import CV from '../../assets/FamzyCV.pdf'
+// import Cert from "../../assets/cert.pdf"
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={Cert} download className='btn'> Download CV </a>
+        <a href={CV} download className='btn'> Download CV </a>
         <a href="#contact" className='btn btn-primary'> Let's Talk </a>
     </div>
     
