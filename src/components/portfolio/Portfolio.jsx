c6e6999b893fe9c86cb5b002c0a9c2afc39e341b
famzyjr <famuyiwaahmed132@gmail.com>
2026-09-25 19:00:02 +0100

    new update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,7 @@ import "./portfolio.css";
 import AOS from "aos";
 import Img2 from "../../assets/Home.JPG";
 import Img3 from "../../assets/wages.PNG";
-import Blog from "../../assets/blog.PNG"
+import Blog from "../../assets/blog.png"
 
 // ..
 AOS.init({
